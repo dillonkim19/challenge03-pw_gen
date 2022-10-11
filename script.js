6ed7e06e093c659ce8dc7fe49a7a3b031bbe9c19
Dillon Kim <dillonkim@Dillons-MacBook-Air.local>
2022-10-11 05:01:43 -0700

    add algo that generates random password

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const lowCase = upCase.map(letter => letter.toLowerCase())
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", "|", ";", ":", "'", ",", ".", "<", ">", "/", "?", "~", "`"]
 
+// Function to get a random integer value
+const getRandomInt = function(max) {
+  return Math.floor(Math.random() * max);
+}
+
 // Function that asks length of password
 const askHowLong = function() {
   
@@ -17,29 +22,33 @@ const askHowLong = function() {
   return passLength
 }
 
-//
+// Function that asks for criteria of password
 const askCriteria = function() {
 
   var criteria = [];
   
+  // Ask if user wants upper case letters
   if (confirm("Would you like to have upper case letters?")) {
     console.log("YES UPPER CASE!!!!");
     criteria.push(upCase);
     console.log(criteria);
   }
 
+  // Ask if user wants lower case letters
   if (confirm("Would you like to have lower case letters?")) {
     console.log("YES LOWER CASE!!!!");
     criteria.push(lowCase);
     console.log(criteria);
   }
 
+  // Ask if user wants numbers
   if (confirm("Would you like to have numbers?")) {
     console.log("YES NUMBERS!!!!");
     criteria.push(numbers);
     console.log(criteria);
   }
 
+  // Ask if user wants special characters
   if (confirm("Would you like to have special characters?")) {
     console.log("YES SPECIAL CHARACTERS!!!!");
     criteria.push(special);
@@ -63,15 +72,25 @@ var generatePassword = function() {
 
   console.log(criteria[3][3]);
 
-  console.log(Math.getRandomInt(3));
+  // Create random password
+  var password = '';
+  for (var i = 0; i < passLength; i++){
 
-  // // Create random password
-  // var password = '';
-  // for (var i = 0; i < passLength; i++){
+    // Chooses a random criteria from criteria array
+    randomCriteria = getRandomInt(criteria.length);
+    console.log(randomCriteria);
+    
+    // Chooses a random character from that criteria
+    randomCharacter = getRandomInt(criteria[randomCriteria].length);
+    console.log(randomCharacter);
+    
+    console.log(criteria[randomCriteria][randomCharacter]);
+    
+    // Adds that character to the password string
+    password = password.concat(criteria[randomCriteria][randomCharacter])
+    console.log(password);
 
-  //   password = password.concat(criteria[])
-
-  // }
+  }
 
 
 }
